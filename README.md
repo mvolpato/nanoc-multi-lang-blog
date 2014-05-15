4aa0a51729e264af5599dd760093d37dff823e27
mvolpato <volpato.michele@gmail.com>
2014-05-16 00:40:21 +0200

    version 0.1

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 nanoc-multi-lang-blog
 =====================
 
-A template for implementing a 2 languages blog using nanoc.
-The template is not ready yet, keep looking at this page. Do not leave it!
+A template for implementing multi language blogs using nanoc (http://nanoc.ws).
 
 See the [compiled result](http://mvolpato.github.io/nanoc-multi-lang-blog/ "nanoc-multi-lang-blog demo").
 
 The demo uses Bootstrap (copyright 2014 Twitter).
+
+Some code has been taken from http://clarkdave.net/2012/02/building-a-static-blog-with-nanoc/
--- lib/default.rb
+++ lib/default.rb
@@ -1,3 +1,56 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
+include Nanoc3::Helpers::Blogging
+include Nanoc3::Helpers::Tagging
+include Nanoc3::Helpers::Rendering
 include Nanoc3::Helpers::LinkTo
+
+module PostHelper
+
+  def get_pretty_date(post)
+    attribute_to_time(post[:created_at]).strftime('%B %-d, %Y')
+  end
+
+  def get_pretty_date_it(post)
+   begin
+    tmp = attribute_to_time(post[:created_at])
+    italianMonths = ["gennaio", "febbraio", "marzo", "aprile", "maggio", "giugno", "luglio", "agosto", "settembre", "ottobre", "novembre", "dicembre"]
+    intMon = tmp.mon
+    intMon -= 1
+    month = italianMonths[intMon]
+    ret = tmp.day.to_s + " " + month + " " + tmp.year.to_s
+   end
+  end
+  
+  def get_post_day(post)
+    attribute_to_time(post[:created_at]).strftime('%e')
+  end  
+  
+  def get_post_month(post)
+    attribute_to_time(post[:created_at]).strftime('%^b')
+  end  
+  
+  def get_post_date(post)
+    attribute_to_time(post[:created_at]).strftime('%B %-d, %Y')
+  end 
+
+end
+
+ def language_code_of(item)
+  # "/en/foo/" becomes "en"
+  ret = (item.identifier.match(/^\/([a-z]{2})\//) || [])[1] 
+  if ret == nil
+    ret = 'en'
+  end
+  return ret
+ end
+
+ def extended_lang(lang)
+  if lang == 'en' 
+    return 'English'
+  else 
+    return 'Italiano' 
+  end
+ end
+
+include PostHelper
